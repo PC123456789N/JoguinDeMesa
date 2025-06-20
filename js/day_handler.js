8f171552b54b9bed0f7ae41f8ba46f3037aa8d37
Kaio Henrique <kaio.lopes@escolar.ifrn.edu.br>
2025-06-20 16:41:56 -0300

    Co-authored-by: BrasilicioHenrique <BrasilicioHenrique@users.noreply.github.com> Co-authored-by: G-Aleixo <G-Aleixo@users.noreply.github.com>

---
+++ js/day_handler.js
@@ -0,0 +1,31 @@
+// max is 6
+let day = 0;
+
+function loadDay0() {
+    console.log("Carregar dias não impe");
+    
+}
+function loadDay1() {
+    console.log("Carregar dias não impe");
+    
+}
+function loadDay2() {
+    console.log("Carregar dias não impe");
+    
+}
+function loadDay3() {
+    console.log("Carregar dias não impe");
+    
+}
+function loadDay4() {
+    console.log("Carregar dias não impe");
+    
+}
+function loadDay5() {
+    console.log("Carregar dias não impe");
+    
+}
+function loadDay6() {
+    console.log("Carregar dias não impe");
+    
+}
