2902c3fdaabe930cd1db2ebc1caf19670a659dd0
brasilicioh <brasiliciohcr@gmail.com>
2025-07-06 23:46:17 -0300

    add arquivo de cutscene e deixar as escolhas grande e negrito

--- js/main.js
+++ js/main.js
@@ -9,18 +9,30 @@ btnGame.forEach(btn => {
             switch (title) {
                 case "Escolha 1":
                     img.src = "../../img/fundos/mesa1.png";
+                    document.getElementById("escolha1").style.fontWeight = "bold";
+                    document.getElementById("escolha1").style.fontSize = "large";
                     break;
                 case "Escolha 2":
                     img.src = "../../img/fundos/mesa2.png";
+                    document.getElementById("escolha2").style.fontWeight = "bold";
+                    document.getElementById("escolha2").style.fontSize = "large";
                     break;
                 case "Escolha 3":
                     img.src = "../../img/fundos/mesa3.png";
+                    document.getElementById("escolha3").style.fontWeight = "bold";
+                    document.getElementById("escolha3").style.fontSize = "large";
                     break;
             }
         })
 
         btn.addEventListener("mouseout", () => {
             img.src = "../../img/fundos/mesa.png";
+            document.getElementById("escolha1").style.fontWeight = "";
+            document.getElementById("escolha1").style.fontSize = "";
+            document.getElementById("escolha2").style.fontWeight = "";
+            document.getElementById("escolha2").style.fontSize = "";
+            document.getElementById("escolha3").style.fontWeight = "";
+            document.getElementById("escolha3").style.fontSize = "";
         })
     }
 })
