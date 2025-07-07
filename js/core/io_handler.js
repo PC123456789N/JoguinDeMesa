346368e1b6339a86ebe5722d1d48eff84ee590e7
brasilicioh <brasiliciohcr@gmail.com>
2025-07-07 10:10:04 -0300

    outroteste

--- js/core/io_handler.js
+++ js/core/io_handler.js
@@ -41,37 +41,37 @@ export function updateHtmlEvent(event) {
     document.getElementById("escolha1").innerHTML = "1. " + event["choices"][0]["text"];
     document.getElementById("escolha2").innerHTML = "2. " + event["choices"][1]["text"];
     document.getElementById("escolha3").innerHTML = "3. " + event["choices"][2]["text"];
-
+    console.log(event["character"]);
     switch (event["character"]) {
         case "Marechal Silva":
-            document.getElementById("imagem2").src = "../img/personagens/marechal.png";
+            document.getElementById("imagem2").src = "JoguinDeMesa/img/personagens/marechal.png";
             break;
         case "Tenente Taísa Torres":
-            document.getElementById("imagem2").src = "../img/personagens/tenente.png";
+            document.getElementById("imagem2").src = "JoguinDeMesa/img/personagens/tenente.png";
             break;
         case "Sargento Márcio":
-            document.getElementById("imagem2").src = "../img/personagens/sargento.png";
+            document.getElementById("imagem2").src = "JoguinDeMesa/img/personagens/sargento.png";
             break;
         case "Economista Síntia":
-            document.getElementById("imagem2").src = "../img/personagens/economista.png";
+            document.getElementById("imagem2").src = "JoguinDeMesa/img/personagens/economista.png";
             break;
         case "Banqueiro Bruno Barros":
-            document.getElementById("imagem2").src = "../img/personagens/banqueiro.png";
+            document.getElementById("imagem2").src = "JoguinDeMesa/img/personagens/banqueiro.png";
             break;
         case "Contador Roberto":
-            document.getElementById("imagem2").src = "../img/personagens/contador.png";
+            document.getElementById("imagem2").src = "JoguinDeMesa/img/personagens/contador.png";
             break;
         case "Recenseadora do IBGE Maria":
-            document.getElementById("imagem2").src = "../img/personagens/recenseadora.png";
+            document.getElementById("imagem2").src = "JoguinDeMesa/img/personagens/recenseadora.png";
             break;
         case "Jornalista Jaime Jason":
-            document.getElementById("imagem2").src = "../img/personagens/jornalista.png";
+            document.getElementById("imagem2").src = "JoguinDeMesa/img/personagens/jornalista.png";
             break;
         case "Guarda Glória Góis":
-            document.getElementById("imagem2").src = "../img/personagens/guarda.png";
+            document.getElementById("imagem2").src = "JoguinDeMesa/img/personagens/guarda.png";
             break;
         case "Karateca Kátia Kane":
-            document.getElementById("imagem2").src = "../img/personagens/karateca.png";
+            document.getElementById("imagem2").src = "JoguinDeMesa/img/personagens/karateca.png";
             break;
     }
 }
